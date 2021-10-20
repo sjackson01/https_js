bfc5581e889900306cb72a2c89398871ac7e563d
sjackson01 <stevenpjackson0@gmail.com>
2021-10-20 10:30:59 -0500

    feat(organize): separate modules

--- app.js
+++ app.js
@@ -1,59 +1,8 @@
-// Require https module
-const https = require("https");
-
-// Print Error Messages
-function printError(error) {
-  console.error(error.message);
-}
-
-function printMessage(username, badgeCount, points) {
-  const message = `${username} has ${badgeCount} totalbadges and ${points} points in JavaScript`;
-  console.log(message);
-}
-
-function getProfile(username) {
-  try {
-    const request = https.get(
-      `https://teamtreehouse.com/${username}.json`,
-      (response) => {
-        if (response.statusCode === 200) {
-          let body = "";
-
-          // Beginning event (Data Event)
-          response.on("data", (data) => {
-            body += data.toString(); // Convert buffer to string
-          });
-
-          // Ending Event (Data Event)
-          response.on("end", () => {
-            try {
-              const profile = JSON.parse(body); // Convert string to object
-              printMessage(
-                username,
-                profile.badges.length,
-                profile.points.JavaScript
-              );
-            } catch (error) {
-              printError(error);
-            }
-          });
-        } else {
-          const message = `There was an error getting the profile for ${username} (${response.statusCode})`;
-          printError(statusCodeError); 
-        }
-      }
-    );
-
-    // Works if no error throw right away but is emitted
-    request.on("error", printError);
-  } catch (error) {
-    printError(error);
-  }
-}
+const profile = require('./profile.js'); 
 
 const users = process.argv.slice(2); // Remove first two elements of array
 
-users.forEach(getProfile);
+users.forEach(profile.get);
 
 /* Equivalent 
 users.forEach(username => {
